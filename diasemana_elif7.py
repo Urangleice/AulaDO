b941f950df5b45b7a69423ee46a64647135fe6b1
Gleice <ussp1@aluno.ifal.edu.br>
2026-04-23 08:17:34 -0300

    Add files via upload

---
+++ diasemana_elif7.py
@@ -0,0 +1,23 @@
+while True:
+    dia = int(input("Digite o n° do dia da semana: "))
+
+    if dia == 1:
+        print("Domingo.")
+    elif dia == 2:
+        print("Segunda")
+    elif dia == 3:
+        print("Terça.")
+    elif dia == 4:
+        print("Quarta.")
+    elif dia == 5:
+        print("Quinta.")
+    elif dia == 6:
+        print("Sexta.")
+    elif dia == 7:
+        print("Sábado.")
+    
+
+while False:
+    if dia != (1,2,3,4,5,6,7):
+        print("Dia inválido.")
+        print("Escolha um n° de 1 até 7.")
